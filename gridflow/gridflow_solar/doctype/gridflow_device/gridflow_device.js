140308a8d54ec76be0130ee3ffa47b89777757a0
samogera <samuel@thematxchange.com>
2026-01-30 08:40:34 +0000

    🚀 Initial Commit: GridFlow Solar V2 (Clean Start)

---
+++ gridflow/gridflow_solar/doctype/gridflow_device/gridflow_device.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, samogera and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("GridFlow Device", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ gridflow/gridflow_solar/doctype/gridflow_device/test_gridflow_device.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, samogera and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestGridFlowDevice(FrappeTestCase):
+	pass
