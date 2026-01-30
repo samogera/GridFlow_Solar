39fb4d9fd4651d9a1eb46dd372c9129b5552ec9a
samogera <samuel@thematxchange.com>
2026-01-30 10:40:48 +0000

    feat: core doctype skeletons for Device, Energy Log, and Carbon Batch

---
+++ gridflow/gridflow_solar/doctype/carbon_credit_batch/carbon_credit_batch.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, samogera and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Carbon Credit Batch", {
+// 	refresh(frm) {
+
+// 	},
+// });
--- gridflow/gridflow_solar/doctype/gridflow_device/gridflow_device.js
+++ gridflow/gridflow_solar/doctype/gridflow_device/gridflow_device.js
@@ -1,8 +1,19 @@
-// Copyright (c) 2026, samogera and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("GridFlow Device", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on('GridFlow Device', {
+    refresh: function(frm) {
+        // Add a custom button to the top of the form
+        frm.add_custom_button(__('Mint Carbon Credits'), function() {
+            frappe.call({
+                method: "gridflow.api.process_stellar_minting",
+                args: {
+                    device_id: frm.doc.name
+                },
+                callback: function(r) {
+                    if (r.message) {
+                        frappe.msgprint(__("Successfully created Batch: " + r.message));
+                        frm.reload_doc();
+                    }
+                }
+            });
+        }, __("Actions"));
+    }
+});
